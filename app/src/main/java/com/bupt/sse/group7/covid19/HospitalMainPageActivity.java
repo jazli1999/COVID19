2a6c1b2baa1df9c0f3befbfbb9a1f9d3fdf4011d
IanLi <jasmine_chieh_li@outlook.com>
2020-04-28 14:39:06 +0800

    refactor hospital mainpage from fragment to activity

---
+++ app/src/main/java/com/bupt/sse/group7/covid19/HospitalMainPageActivity.java
@@ -0,0 +1,105 @@
+package com.bupt.sse.group7.covid19;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HospitalMainPageActivity extends AppCompatActivity {
+    private JsonObject hospital;
+    private HospitalContactFragment contactFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hospital_main_page);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getWindow().setNavigationBarColor(Color.TRANSPARENT);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+
+        initView();
+        initData();
+        updateView();
+    }
+
+    // TODO call initView()
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    private void initData() {
+        Thread thread = getHospitalInfo(1);
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void updateView() {
+        ((TextView)this.findViewById(R.id.hospital_name)).setText(hospital.get("name").getAsString());
+        ((TextView)this.findViewById(R.id.hospital_desc)).setText(MessageFormat.format("剩余床位  轻症 {0} | 重症 {1}",
+                hospital.get("mild_left").getAsString(),
+                hospital.get("severe_left").getAsString()));
+        updateHospitalContact();
+    }
+
+
+    private void updateHospitalContact() {
+        contactFragment.setTel(hospital.get("tel").getAsString());
+        contactFragment.setAddress(hospital.get("address").getAsString());
+        contactFragment.setPeople(hospital.get("contact").getAsString());
+    }
+
+    private Thread getHospitalInfo(int h_id) {
+        Map<String, String> args = new HashMap<>();
+        args.put("h_id", String.valueOf(h_id));
+        Thread thread = new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        hospital = DBConnector.getHospitalById(args).get(0).getAsJsonObject();
+                    }
+                }
+        );
+        thread.start();
+        return thread;
+    }
+
+    private void initView() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        contactFragment = new HospitalContactFragment();
+        FragmentTransaction tran = fragmentManager.beginTransaction();
+        tran.add(R.id.hosp_main_content, contactFragment);
+        tran.commit();
+    }
+
+}
