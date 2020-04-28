c5b9518cb063ac5ed9de5cb5592b88f2c15a5bf2
IanLi <jasmine_chieh_li@outlook.com>
2020-04-28 13:09:32 +0800

    HospitalMainPage: add hospital contact card

---
+++ app/src/main/java/com/bupt/sse/group7/covid19/HospitalContactFragment.java
@@ -0,0 +1,59 @@
+package com.bupt.sse.group7.covid19;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+public class HospitalContactFragment extends Fragment {
+    private View view;
+    private String people;
+    private String tel;
+    private String address;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hospital_contact, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        initView();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.view = view;
+    }
+
+    private void initView() {
+        Log.d("contact", tel);
+        ((TextView) view.findViewById(R.id.hosp_tel)).setText(tel);
+        ((TextView) view.findViewById(R.id.hosp_address)).setText(address);
+        ((TextView) view.findViewById(R.id.hosp_inCharge)).setText(people);
+    }
+
+
+    public void setPeople(String people) {
+        this.people = people;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
